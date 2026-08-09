12e20c0c08ed79bfc9da8d2574a09cc8b6e08446
jhseo <jhwest.dev@gmail.com>
2026-08-10 01:33:20 +0900

    chore: CORS 허용 origin에 배포 도메인 추가

--- main.py
+++ main.py
@@ -25,6 +25,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://localhost:5173",
+        "https://bluenote-web-five.vercel.app"
     ],
     allow_credentials=True,
     allow_methods=["*"],
